5cca625be464e5a94a27100f6abcf4043361a23a
Jiri Hollan <jiri.hollan@gmail.com>
2024-03-01 20:00:50 +0100

    Zaixan odkaz na navodila in folder navodila

--- skupne/menu-items.php
+++ skupne/menu-items.php
@@ -1,6 +1,6 @@
 <?php
      $a0= '
-	   <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>
+	  <!-- <li><a href="../navodila/navodilaKovid.php">Navodila</a> </li>-->
      <!--  <li><a href="../pregled/zdravnik.php">Pregled</a> </li>-->
 	     <li><span onclick=sbFunction("spomin")>Pregled</span>  </li>
  <!-- <li><a href="https://docs.google.com/spreadsheets/d/1XJEgArtGUnVuhZ1m4KmNVgukA-WPdkwzV4gdR0r4JbY/edit#gid=0">koledar</a></li>-->
