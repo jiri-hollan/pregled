3008f5c1c2d973a3e65da3f31fe61b89ef227002
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-27 20:36:29 +0200

    reset->RESET

--- pregled/zdravnik.php
+++ pregled/zdravnik.php
@@ -51,7 +51,7 @@ echo '<button id="buttonDomov" onclick="window.location.href=' . "'" . $home . "
   listaZdravnikovFunction(zdravListX);
 //alert(localStorage.getItem("bazeBolnisnice"));
   </script>
-     <span class="" id="odjava" onclick="odjavaFunction()">reset</span>
+     <span class="" id="odjava" onclick="odjavaFunction()">RESET</span>
  </div>	 
 </body>
 </html>
