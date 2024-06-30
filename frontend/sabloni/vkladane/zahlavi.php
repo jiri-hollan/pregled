6efe6e9479e1b7009593162da399a54b5c285e2b
Jiri Hollan <jiri.hollan@gmail.com>
2024-06-30 12:43:56 +0200

    anestizspremenjen na pregled

--- frontend/sabloni/vkladane/zahlavi.php
+++ frontend/sabloni/vkladane/zahlavi.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Anestiz</title>
+<title>Pregled</title>
 <link rel="shortcut icon" href="../favicon.ico?<?php echo time(); ?>">
 <link rel="stylesheet" href="sabloni/css/zahlavi.css?<?php echo time(); ?>">
 <link rel="stylesheet" href="../css/menuFile.css?<?php echo time(); ?>">
